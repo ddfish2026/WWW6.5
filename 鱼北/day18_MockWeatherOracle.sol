ad42c65001bf78d29e49de021629d810341da965
ddfish2026 <ddfish8@163.com>
2026-03-22 10:48:29 +0000

    correct18

--- 鱼北/day18_MockWeatherOracle.sol
+++ 鱼北/day18_MockWeatherOracle.sol
@@ -1,7 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+// 手动定义 Chainlink 接口（替代 import）
+interface AggregatorV3Interface {
+    function decimals() external view returns (uint8);
+    function description() external view returns (string memory);
+    function version() external view returns (uint256);
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+}
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MockWeatherOracle is AggregatorV3Interface, Ownable {
